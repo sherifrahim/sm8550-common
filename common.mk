c30e80b9cfa3225071df6514f480388c236c4766
J0SH1X <aljoshua.hell@gmail.com>
2024-01-28 00:16:37 +0100

    Weaver: build weaver from src

--- common.mk
+++ common.mk
@@ -443,7 +443,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libcrypto.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcrypto-v33.so
 
-PRODUCT_EXTRA_VNDK_VERSIONS := 33
+# Weaver
+PRODUCT_PACKAGES += \
+    android.hardware.weaver@1.0.vendor
 
 # WiFi
 PRODUCT_PACKAGES += \
