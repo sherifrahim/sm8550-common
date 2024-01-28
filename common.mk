5326a43e90cd256c9bb4e698ccfd3f204694020f
J0SH1X <aljoshua.hell@gmail.com>
2024-01-28 12:54:08 +0100

    Dont build libcodec2_vndk

--- common.mk
+++ common.mk
@@ -267,7 +267,6 @@ PRODUCT_PACKAGES += \
     libcodec2_hidl@1.0.vendor \
     libcodec2_hidl@1.1.vendor:64 \
     libcodec2_hidl@1.2.vendor:64 \
-    libcodec2_vndk.vendor \
     libmm-omxcore \
     libpalclient \
     android.hardware.media.c2@1.2.vendor
